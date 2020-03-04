7a1963d7ef00e4ae0a8c7e51b8302b19157e7b5c
Marco Peereboom <marco@peereboom.us>
2020-03-04 15:35:05 -0600

    Also validate TSPEND

--- blockchain/stake/treasury_test.go
+++ blockchain/stake/treasury_test.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/chaincfg/v2"
 	"github.com/decred/dcrd/dcrutil/v3"
@@ -179,6 +180,63 @@ func TestTreasuryIsFunctions(t *testing.T) {
 			is:       IsTreasuryBase,
 			expected: true,
 			check:    checkTreasuryBase,
+		},
+		{
+			name: "tspend",
+			createTx: func() *wire.MsgTx {
+				// OP_RETURN <32 byte random>
+				payload := make([]byte, chainhash.HashSize)
+				_, err := rand.Read(payload)
+				if err != nil {
+					panic(err)
+				}
+				builder := txscript.NewScriptBuilder()
+				builder.AddOp(txscript.OP_RETURN)
+				builder.AddData(payload)
+				opretScript, err := builder.Script()
+				if err != nil {
+					panic(err)
+				}
+				msgTx := wire.NewMsgTx()
+				msgTx.AddTxOut(wire.NewTxOut(0, opretScript))
+
+				// OP_TGEN
+				p2shOpTrueAddr, err := dcrutil.NewAddressScriptHash([]byte{txscript.OP_TRUE},
+					chaincfg.MainNetParams())
+				p2shOpTrueScript, err := txscript.PayToAddrScript(p2shOpTrueAddr)
+				if err != nil {
+					panic(err)
+				}
+				script := make([]byte, len(p2shOpTrueScript)+1)
+				script[0] = txscript.OP_TGEN
+				copy(script[1:], p2shOpTrueScript)
+				msgTx.AddTxOut(wire.NewTxOut(0, script))
+
+				// tspend
+				builder = txscript.NewScriptBuilder()
+				builder.AddOp(txscript.OP_TSPEND)
+				tspendScript, err := builder.Script()
+				if err != nil {
+					panic(err)
+				}
+				msgTx.AddTxIn(&wire.TxIn{
+					// Stakebase transactions have no
+					// inputs, so previous outpoint is zero
+					// hash and max index.
+					PreviousOutPoint: *wire.NewOutPoint(&chainhash.Hash{},
+						wire.MaxPrevOutIndex, wire.TxTreeRegular),
+					Sequence:        wire.MaxTxInSequenceNum,
+					BlockHeight:     wire.NullBlockHeight,
+					BlockIndex:      wire.NullBlockIndex,
+					SignatureScript: tspendScript,
+				})
+				t.Logf("%v", spew.Sdump(msgTx))
+
+				return msgTx
+			},
+			is:       IsTSpend,
+			expected: true,
+			check:    checkTSpend,
 		},
 	}
 
