e2a5a38072b6a8bec2d7935867b59f4c5162b076
Marco Peereboom <marco@peereboom.us>
2020-07-13 15:04:53 -0500

    Bring back invalid opcode test

--- txscript/script_test.go
+++ txscript/script_test.go
@@ -351,6 +351,13 @@ func TestRemoveOpcodeByData(t *testing.T) {
 				"0x01020304"),
 		},
 		{
+			name:   "invalid opcode",
+			before: []byte{OP_UNKNOWN240},
+			remove: []byte{1, 2, 3, 4},
+			after:  []byte{OP_UNKNOWN240},
+		},
+
+		{
 			name:   "invalid length (instruction)",
 			before: []byte{OP_PUSHDATA1},
 			remove: []byte{1, 2, 3, 4},
