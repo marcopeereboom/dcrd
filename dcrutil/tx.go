43b787ba4bcb16a30e1858c07569fa682fdf1666
Marco Peereboom <marco@peereboom.us>
2020-07-20 09:02:28 -0500

    ETOOMANYIN

--- dcrutil/tx.go
+++ dcrutil/tx.go
@@ -84,7 +84,7 @@ func (t *Tx) Version() uint16 {
 	return t.msgTx.Version
 }
 
-// SetVersion sets the version of the transaction in within a block.
+// SetVersion sets the version of the transaction within a block.
 func (t *Tx) SetVersion(version uint16) {
 	t.msgTx.Version = version
 }
